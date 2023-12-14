be3be98f6be3fa17049fde35b05ae119d8d1c674
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-14 21:37:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+if (s === '') {
+    return '';
+  }
+
+  const spaceIndex = s.indexOf(' ');
+
+  if (spaceIndex === -1) {
+    return s; // Return the entire string if there is no space.
+  }
+
+  return s.substring(0, spaceIndex);	
 }
 
 // Do not change the code below
